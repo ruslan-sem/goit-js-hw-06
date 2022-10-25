ec3b96e589fe184b83038275a688b5a9d846ac38
Ruslan Samoylenko <ruslan.sem@gmail.com>
2022-10-25 09:40:10 +0300

    9 заданий

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.querySelector('body');
+const span = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+btn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  span.textContent = color;
+  body.style.backgroundColor = color;
+});
